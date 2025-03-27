83076bb0cada9b8e354ffd684c88385a7d3b17a4
Bawar Zrng Jalal <bawarzring@gmail.com>
2025-03-27 14:57:18 +0300

    creating eks

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -145,89 +145,89 @@ resource "aws_security_group" "eks-cluster" {
     }
 }
 
-# resource "aws_iam_role" "eks-role" {
-#   name = "eks-role-jenkins"
-#   assume_role_policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Principal": {
-#         "Service": "eks.amazonaws.com"
-#       },
-#       "Action": "sts:AssumeRole"
-#     }
-#   ]
-# }
-# EOF
-# }
+resource "aws_iam_role" "eks-role" {
+  name = "eks-role-jenkins-new"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
 
-# resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-#   role       = aws_iam_role.eks-role.name
-# }
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks-role.name
+}
 
-# resource "aws_iam_role_policy_attachment" "eks_service_policy" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-#   role       = aws_iam_role.eks-role.name
-# }
+resource "aws_iam_role_policy_attachment" "eks_service_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks-role.name
+}
 
-# resource "aws_eks_cluster" "eks-cluster" {
-#   name     = "eks-cluster"
-#   role_arn = aws_iam_role.eks-role.arn
-#   vpc_config {
-#     subnet_ids = [aws_subnet.private-AZ1.id, aws_subnet.private-AZ2.id]
-#     security_group_ids = [aws_security_group.eks-cluster.id]
-#   }
-# }   
+resource "aws_eks_cluster" "eks-cluster" {
+  name     = "eks-cluster"
+  role_arn = aws_iam_role.eks-role.arn
+  vpc_config {
+    subnet_ids = [aws_subnet.private-AZ1.id, aws_subnet.private-AZ2.id]
+    security_group_ids = [aws_security_group.eks-cluster.id]
+  }
+}   
 
-# resource "aws_iam_role" "eks-node-role" {
-#   name = "eks-node-role-jenkins"
-#   assume_role_policy = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Principal": {
-#         "Service": "ec2.amazonaws.com"
-#       },
-#       "Action": "sts:AssumeRole"
-#     }
-#   ]
-# }
-# EOF
-# }
+resource "aws_iam_role" "eks-node-role" {
+  name = "eks-node-role-jenkins-new"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
 
-# resource "aws_iam_role_policy_attachment" "eks_node_policy" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-#   role       = aws_iam_role.eks-node-role.name
-# }
+resource "aws_iam_role_policy_attachment" "eks_node_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks-node-role.name
+}
 
-# resource "aws_iam_role_policy_attachment" "eks_ecr_read_only" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-#   role       = aws_iam_role.eks-node-role.name
-# }
+resource "aws_iam_role_policy_attachment" "eks_ecr_read_only" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks-node-role.name
+}
 
-# resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-#   role       = aws_iam_role.eks-node-role.name
-# }
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks-node-role.name
+}
 
-# resource "aws_eks_node_group" "eks-node-group" {
-#   cluster_name    = aws_eks_cluster.eks-cluster.name
-#   node_group_name = "eks-node-group"
-#   node_role_arn   = aws_iam_role.eks-node-role.arn
-#   subnet_ids      = [aws_subnet.private-AZ1.id, aws_subnet.private-AZ2.id]
-#   scaling_config {
-#     desired_size = 2
-#     max_size     = 2
-#     min_size     = 2
-#   }
-#   instance_types = ["t3.medium"]
-#   ami_type       = "AL2_x86_64"
-# }
+resource "aws_eks_node_group" "eks-node-group" {
+  cluster_name    = aws_eks_cluster.eks-cluster.name
+  node_group_name = "eks-node-group"
+  node_role_arn   = aws_iam_role.eks-node-role.arn
+  subnet_ids      = [aws_subnet.private-AZ1.id, aws_subnet.private-AZ2.id]
+  scaling_config {
+    desired_size = 2
+    max_size     = 2
+    min_size     = 2
+  }
+  instance_types = ["t3.medium"]
+  ami_type       = "AL2_x86_64"
+}
 
 resource "aws_security_group" "efs_sg" {
   name        = "efs-security-group-1"
@@ -292,27 +292,27 @@ resource "aws_efs_access_point" "jenkins-efs-access-point" {
   }
 }
 
-# resource "aws_iam_policy" "efs_access_policy" {
-#   name        = "EFSAccessPolicy"
-#   description = "IAM policy to allow EKS nodes to access EFS"
-#   policy      = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Effect   = "Allow"
-#         Action   = [
-#           "elasticfilesystem:DescribeFileSystems",
-#           "elasticfilesystem:DescribeMountTargets",
-#           "elasticfilesystem:ClientMount",
-#           "elasticfilesystem:ClientWrite"
-#         ]
-#         Resource = "*"
-#       }
-#     ]
-#   })
-# }
+resource "aws_iam_policy" "efs_access_policy" {
+  name        = "EFSAccessPolicy1"
+  description = "IAM policy to allow EKS nodes to access EFS"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "elasticfilesystem:DescribeFileSystems",
+          "elasticfilesystem:DescribeMountTargets",
+          "elasticfilesystem:ClientMount",
+          "elasticfilesystem:ClientWrite"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
 
-# resource "aws_iam_role_policy_attachment" "efs_access_policy_attachment" {
-#   policy_arn = aws_iam_policy.efs_access_policy.arn
-#   role       = aws_iam_role.eks-node-role.name
-# }
+resource "aws_iam_role_policy_attachment" "efs_access_policy_attachment" {
+  policy_arn = aws_iam_policy.efs_access_policy.arn
+  role       = aws_iam_role.eks-node-role.name
+}
