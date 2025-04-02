2253c285296aab4b3666ca3a01f82a47ef8c8e5f
Bawar Zrng Jalal <bawarzring@gmail.com>
2025-04-02 14:22:09 +0300

    and new ingress

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -150,6 +150,13 @@ resource "aws_security_group" "eks-cluster" {
   }
 
   ingress {
+    from_port = 8080
+    to_port   = 8080
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
